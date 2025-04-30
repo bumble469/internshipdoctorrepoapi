a4bdbf75a9dcbfa137a3f39eace1db4cae490587
bumble469 <bumble469@gmail.com>
2025-04-30 15:36:58 +0530

    deployment

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const cors = require('cors');
-const getDoctorDetails = require('./routes/ListDoctorRoute');
-const addDoctor = require("./routes/AddDoctorRoute");
-const allowedorigins = process.env.FRONTEND_URL.split(',');
 
+const cors = require("cors");
+
+// ✅ Use environment variable
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+// ✅ Apply CORS before routes
 app.use(cors({
-    origin : allowedorigins,
-    credentials: true,
+  origin: FRONTEND_URL,
+  credentials: true // if you need cookies or auth headers
 }));
 
 app.use(express.json());
-app.use("/api", getDoctorDetails, addDoctor);
 
-const PORT = process.env.PORT || 5000;
-;
+// ✅ Register routes
+const getDoctorDetails = require('./routes/ListDoctorRoute');
+const addDoctor = require("./routes/AddDoctorRoute");
+app.use("/api", getDoctorDetails);
+app.use("/api", addDoctor);
+
+// ✅ Start server
+const PORT = process.env.DB_PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
